52eb210ae3935c7eded9bb5259f8ac546c0b4ebc
rozerAdda247 <bag.rozer@adda247.com>
2024-12-30 10:47:22 +0530

    the https server

--- src/hooks/http.ts
+++ src/hooks/http.ts
@@ -1,6 +1,6 @@
 console.log(process.env.NODE_ENV)
 const isDevelopement = process.env.NODE_ENV === "development";
 export const baseURL = isDevelopement
-  ? "http://localhost:3006/api/v1"
-  : "http://174.138.123.193:3006/api/v1";
+  ? "https://localhost:3006/api/v1"
+  : "https://174.138.123.193:3006/api/v1";
 // export const baseURL = "http://174.138.123.193:3006/api/v1";
